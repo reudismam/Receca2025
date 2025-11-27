4776ce6f68977f2ed2bb94b422d963fea67fbbf5
reudismam <reudismam@gmail.com>
2025-11-27 20:15:48 -0300

    Add Streamlit app to visualize COVID-19 data

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df=pd.read_csv('WHO_time_series.csv')
+
+df['Date_reported'] = pd.to_datetime(df['Date_reported'])
+
+fig1 = px.line(df, 
+               x = 'Date_reported', 
+               y = 'Cumulative_cases', 
+               color = 'Country', 
+               title = 'Número de casos acumulados - Covid-19')
+
+fig1.update_layout(xaxis_title = 'Data', yaxis_title = 'Número de Casos Acumulados')
+
+fig1.show()
+
+st.plotly_chart(fig1, use_container_width=True)
